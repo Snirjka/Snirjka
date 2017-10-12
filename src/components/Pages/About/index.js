c951b738ca63de7d25fb5b934391edf733a47863
snirjka <snirjka@gmail.com>
2017-10-12 18:22:04 +0300

    going up!

--- src/components/Pages/About/index.js
+++ src/components/Pages/About/index.js
@@ -17,9 +17,9 @@ export default class About extends Component {
                         I am specialising in front-end - HTML/CSS/JS.<br /><br />
                         Used libraries such as - React+Redux/jQuery/Angular.<br />
                         I know back-end as well - PHP/MYSQL/NODEJS, Also knows Symfony/Doctrine.<br /><br />
-                        You can check the code to the site on:<br />
-                        <b>https://github.com/Snirjka/Snirjka.github.io</b><br />
-                        Enjoy!
+                        You can check the code to the site<br />
+                        <a target='self' href="https://github.com/Snirjka/Snirjka">HERE!</a><br />
+                        Enjoy.
                         </p>
                     <span className='quote h1'>&lt;/p&gt;</span><br />
                     <span className='quote body'>&lt;/body&gt;</span><br />
