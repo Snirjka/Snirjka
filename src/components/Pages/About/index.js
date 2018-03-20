602bdb2f13083b82a904fd8155d55539d6eaf204
Snirjka <snirjka@gmail.com>
2018-03-20 22:00:11 +0200

    Update index.js

--- src/components/Pages/About/index.js
+++ src/components/Pages/About/index.js
@@ -16,7 +16,7 @@ export default class About extends Component {
                         In the last couple of years I am studying Programming & web development, <br />
                         and I'm currently working in Groupon. <br />
                         I'm having the most fun working on the client side  - ReactJS | Anuglar >= 2,<br />
-                        although i got some experience with backend - Laravel | Nodejs<br /><br />
+                        although i got some experience with backend - PHP | Nodejs<br /><br />
                         You can check the code to the site<br />
                         <a target='self' href="https://github.com/Snirjka/Snirjka.github.io">HERE!</a><br />
                         Enjoy.
